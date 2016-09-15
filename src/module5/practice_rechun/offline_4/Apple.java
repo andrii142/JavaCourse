3504e99aeecb00dfbb487e711fca668b4439a4a8
zetokz <iamzetok@gmail.com>
2016-09-15 09:52:28 +0300

    Added code from offline 4

---
+++ src/module5/practice_rechun/offline_4/Apple.java
@@ -0,0 +1,28 @@
+package module5.practice_rechun.offline_4;
+
+/**
+ * Created by zetokz on 12.09.16.
+ */
+public class Apple {
+
+    private int weight;
+
+    public Apple(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Apple apple = (Apple) o;
+
+        return weight == apple.weight;
+    }
+
+    @Override
+    public int hashCode() {
+        return weight;
+    }
+}
---
+++ src/module5/practice_rechun/offline_4/View.java
@@ -0,0 +1,12 @@
+package module5.practice_rechun.offline_4;
+
+/**
+ * Created by zetokz on 12.09.16.
+ */
+public abstract class View {
+
+    public abstract void show();
+    public abstract void hide();
+
+
+}
